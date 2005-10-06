4f65fb27c04f0a6c6be54927aca8714e1497a186
Raphaël Slinckx <rslinckx@src.gnome.org>
2005-10-06 16:36:43 +0000

    *** empty log message ***

---
+++ deskbar/applet_keybinder.py
@@ -0,0 +1,28 @@
+import deskbar, deskbar.keybinder
+
+def on_global_keybinding(applet):
+	# We want to grab focus here
+	applet.applet.request_focus(deskbar.keybinder.tomboy_keybinder_get_current_event_time())
+	applet.entry.get_entry().grab_focus()
+
+class AppletKeybinder:
+	def __init__(self, applet):
+		self.applet = applet
+		
+		# Set and retreive global keybinding from gconf
+		self.keybinding = deskbar.GCONF_CLIENT.get_string(deskbar.GCONF_KEYBINDING)
+		deskbar.GCONF_CLIENT.notify_add(deskbar.GCONF_KEYBINDING, lambda x, y, z, a: self.on_config_keybinding(z.value))
+		self.bind()
+	
+	def on_config_keybinding(self, value=None):
+		if value != None and value.type == gconf.VALUE_STRING:
+			self.unbind()
+			self.keybinding = value.get_string()
+			self.bind()
+			
+	def bind(self):
+		print 'Binding Global shortcut %s to focus the deskbar' % self.keybinding
+		deskbar.keybinder.tomboy_keybinder_bind(self.keybinding, on_global_keybinding, self.applet)
+
+	def unbind(self):
+		deskbar.keybinder.tomboy_keybinder_unbind(self.keybinding, on_global_keybinding)
