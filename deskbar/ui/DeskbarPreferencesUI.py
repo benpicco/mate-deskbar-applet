c2a82444bef27625306491194e4359021ae48fdc
Raphael Slinckx <rslinckx@cvs.gnome.org>
2006-04-09 17:30:32 +0000

    Warn the user when the keybinding is not formatted properly Patch by
    2006-04-09  Raphael Slinckx  <rslinckx@cvs.gnome.org>
    
    	* deskbar/ui/DeskbarPreferencesUI.py:
    	Warn the user when the keybinding is not formatted properly
    	Patch by Sebastian Pölsterl <marduk@k-d-w.org>

--- deskbar/ui/DeskbarPreferencesUI.py
+++ deskbar/ui/DeskbarPreferencesUI.py
@@ -32,7 +32,7 @@ class DeskbarPreferencesUI:
 		self.fixed_width_radio = self.glade.get_widget("fixed_width_radio")
 		
 		self.keyboard_shortcut_entry = self.glade.get_widget("keyboard_shortcut_entry")
-		self.keyboard_shortcut_entry.connect('changed', self.on_keyboard_shortcut_entry_changed, applet)
+		self.keyboard_shortcut_entry.connect('focus-out-event', self.on_keyboard_shortcut_focus_out_event, applet)
 		self.keybinding_notify_id = deskbar.GCONF_CLIENT.notify_add(applet.prefs.GCONF_KEYBINDING, lambda x, y, z, a: self.on_config_keybinding(z.value))
 		
 		container = self.glade.get_widget("handlers")
@@ -148,8 +148,17 @@ class DeskbarPreferencesUI:
 	def on_spin_width_change(self, spinner, applet):
 		deskbar.GCONF_CLIENT.set_int(applet.prefs.GCONF_WIDTH, int(spinner.get_value()))
 	
-	def on_keyboard_shortcut_entry_changed(self, entry, applet):
-		deskbar.GCONF_CLIENT.set_string(applet.prefs.GCONF_KEYBINDING, entry.get_text())
+	def on_keyboard_shortcut_focus_out_event(self, entry, event, applet):
+		keyval, modifier = gtk.accelerator_parse(entry.get_text())
+		if gtk.accelerator_valid(keyval, modifier):
+			deskbar.GCONF_CLIENT.set_string(applet.prefs.GCONF_KEYBINDING, entry.get_text())
+		else:
+			error = gtk.MessageDialog(parent=self.dialog,
+							    type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK,
+							    message_format= _('Invalid shortcut provided')
+							   )
+			error.run()
+			error.destroy()
 	
 	def on_more_button_clicked(self, button):
 		if self.more_button_callback != None:
