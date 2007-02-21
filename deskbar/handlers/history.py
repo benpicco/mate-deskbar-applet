dfcb7fcd018fcffb32c9209132cae2373cb743ac
Mikkel Kamstrup Erlandsen <kamstrup@src.gnome.org>
2007-02-21 23:55:17 +0000

    Fix bug #397838. Sort history matches by last use.
    svn path=/trunk/; revision=1170

--- deskbar/handlers/history.py
+++ deskbar/handlers/history.py
@@ -1,5 +1,7 @@
 from gettext import gettext as _
 
+import gtk
+
 import deskbar.Handler
 from deskbar.DeskbarHistory import get_deskbar_history
 from deskbar.defs import VERSION
@@ -15,16 +17,32 @@ HANDLERS = {
 class HistoryHandler(deskbar.Handler.Handler):
 	def __init__(self):
 		deskbar.Handler.Handler.__init__(self, "stock_redo")
+	
+	def _get_history_order (self):
+		col_id, order = get_deskbar_history ().get_sort_column_id ()
+		return order
 		
 	def query(self, query):
 		result = []
+		
+		# We need to take the position of the applet into consideration
+		# this can be identified through the history sorting
+		priority = len (get_deskbar_history())
+		inc_priority = None
+		if self._get_history_order() == gtk.SORT_DESCENDING :
+			inc_priority = lambda x : x - 1
+		else:
+			inc_priority = lambda x : x + 1
+			
 		for text, match in get_deskbar_history():
 			if text.startswith(query):
 				match.get_category = lambda: "history"
-				
-				# Beware of the infinite recursion here !
-				match_prio = match.get_priority()[1]
-				match.get_priority = lambda: (self.get_priority(), match_prio)
+								
+				# Spare my life for overriding a private variable, but overriding
+				# the method doesn't work . ie:
+				# match.get_priority = lambda: (self.get_priority(), priority)
+				match._priority = priority
 				result.append((text, match))
+				priority = inc_priority (priority)
 		
 		return result
