ca7ed1527defe7e0ab4ccf779f3abf96f2799505
Sebastian Polsterl <sebp@src.gnome.org>
2008-02-03 16:18:54 +0000

    Added documenters to About dialog
    svn path=/trunk/; revision=1913

--- deskbar/ui/About.py
+++ deskbar/ui/About.py
@@ -16,21 +16,19 @@ gtk.about_dialog_set_url_hook(on_url)
 def show_about(parent):
     about = gtk.AboutDialog()
     infos = {
-        "name" : _("Deskbar"),
-        "logo-icon-name" : "deskbar-applet",
-        "version" : VERSION,
+        "authors": ["Nigel Tao <nigel.tao@myrealbox.com>",
+                    "Raphael Slinckx <raphael@slinckx.net>",
+                    "Mikkel Kamstrup Erlandsen <kamstrup@daimi.au.dk>",
+                    "Sebastian Pölsterl <marduk@k-d-w.org>"],
         "comments" : _("An all-in-one action bar."),
         "copyright" : "Copyright © 2004-2008\nNigel Tao, Raphael Slinckx, Mikkel Kamstrup Erlandsen, Sebastian Pölsterl.",
+        "documenters" : ["Phil Bull <philbull@gmail.com>", "Qing Gan"],
+        "logo-icon-name" : "deskbar-applet",
+        "name" : _("Deskbar"),
+        "version" : VERSION,
         "website" : "http://raphael.slinckx.net/deskbar",
-        "website-label" : _("Deskbar Website"),
+        "website-label" : _("Deskbar Website"),   
     }
-
-    about.set_authors(["Nigel Tao <nigel.tao@myrealbox.com>",
-                       "Raphael Slinckx <raphael@slinckx.net>",
-                       "Mikkel Kamstrup Erlandsen <kamstrup@daimi.au.dk>",
-                       "Sebastian Pölsterl <marduk@k-d-w.org>"])
-#    about.set_artists([])
-#    about.set_documenters([])
     
     #translators: These appear in the About dialog, usual format applies.
     about.set_translator_credits( _("translator-credits") )
