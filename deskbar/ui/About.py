e6222fceea5d80c48f2ef96c3257ec3ff7638bbd
Sebastian Polsterl <sebp@src.gnome.org>
2008-11-30 20:11:37 +0000

    Updated URL of Deskbar homepage
    svn path=/trunk/; revision=2490

--- deskbar/ui/About.py
+++ deskbar/ui/About.py
@@ -28,7 +28,7 @@ def show_about(parent):
         "logo-icon-name" : "deskbar-applet",
         "name" : _("Deskbar"),
         "version" : VERSION,
-        "website" : "http://www.gnome.org/projects/deskbar-applet/",
+        "website" : "http://projects.gnome.org/deskbar-applet/",
         "website-label" : _("Deskbar Website"),   
     }
     
